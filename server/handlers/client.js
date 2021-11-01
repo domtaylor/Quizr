1f0e8bf251333e50e5e9c9bc1d6bb06dea1c8898
Dom Taylor <dominick.taylor@gmail.com>
2021-11-01 10:58:09 +0000

    hardcode shop in client

--- server/handlers/client.js
+++ server/handlers/client.js
@@ -1,17 +1,15 @@
 import ApolloClient from "apollo-boost";
 
-export const createClient = (shop, accessToken) => {
+export const createClient = (accessToken) => {
   return new ApolloClient({
-    uri: `https://${shop}/admin/api/2019-10/graphql.json`,
-    request: operation => {
+    uri: `https://skin-care-quiz.myshopify.com/admin/api/2019-10/graphql.json`,
+    request: (operation) => {
       operation.setContext({
         headers: {
           "X-Shopify-Access-Token": accessToken,
-          "User-Agent": `shopify-app-node ${
-            process.env.npm_package_version
-          } | Shopify App CLI`
-        }
+          "User-Agent": `shopify-app-node ${process.env.npm_package_version} | Shopify App CLI`,
+        },
       });
-    }
+    },
   });
 };
