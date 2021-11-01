3124eec73befa5fb30bca248c5decc1007580803
Dom Taylor <dominick.taylor@gmail.com>
2021-11-01 10:11:38 +0000

    remove shop param from index

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,52 @@
-require('isomorphic-fetch');
-import React from 'react'
-import { connect } from 'react-redux'
-import GeneralForm from '../components/GeneralForm';
-import { getSettings, saveSettings } from '../store'
-import { Loading } from '@shopify/polaris';
-
+require("isomorphic-fetch");
+import React from "react";
+import { connect } from "react-redux";
+import GeneralForm from "../components/GeneralForm";
+import { getSettings, saveSettings } from "../store";
+import { Loading } from "@shopify/polaris";
 
 class Index extends React.Component {
-    static async getInitialProps(ctx) {
-        return {
-            query: ctx.query,
-            shop: ctx.params
-        }
-      }
-
-    componentDidMount(){
-        this.props.getSettings(this.props.query.shop)
-    }
-
-    render() {
-        return <div>
-            {this.props.isLoaded
-                ? <GeneralForm {...this.props.settings} save={this.save} isSaving={this.props.isSaving}/>
-                : <Loading />
-            }
-      </div>
+  static async getInitialProps(ctx) {
+    return {
+      query: ctx.query,
     };
+  }
 
-    save = (data) => { this.props.saveSettings(this.props.query.shop, data) }
+  componentDidMount() {
+    this.props.getSettings();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.isLoaded ? (
+          <GeneralForm
+            {...this.props.settings}
+            save={this.save}
+            isSaving={this.props.isSaving}
+          />
+        ) : (
+          <Loading />
+        )}
+      </div>
+    );
+  }
+
+  save = (data) => {
+    this.props.saveSettings(data);
+  };
 }
 
 //Connect Redux
 const mapStateToProps = (state) => {
-    return {
-        isLoaded: state.isLoaded,
-        isSaving: state.isSaving,
-        settings: state.settings
-    }
-}
-const mapDispatchToProps = { getSettings, saveSettings }
+  return {
+    isLoaded: state.isLoaded,
+    isSaving: state.isSaving,
+    settings: state.settings,
+  };
+};
+const mapDispatchToProps = { getSettings, saveSettings };
 
-const connectedIndex = connect(mapStateToProps, mapDispatchToProps)(Index)
+const connectedIndex = connect(mapStateToProps, mapDispatchToProps)(Index);
 
 export default connectedIndex;
